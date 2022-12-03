8014775c500cb92f4df013ede3ddb6565164ec13
prateek004soni <111335355+prateek004soni@users.noreply.github.com>
2022-12-04 00:00:08 +0530

    Add files via upload

---
+++ childPOC.js
@@ -0,0 +1,49 @@
+import { LightningElement,api } from 'lwc';
+
+export default class ChildPOC extends LightningElement {
+
+    className = "greenBar";
+    className2 = "greenBar";
+    className3 = "greenBar";
+    className4 = "greenBar";
+    
+    @api changeBarColor() {
+
+        this.className = "redBar";
+    }
+
+    @api changeBarColor2() {
+
+        this.className2 = "redBar";
+    }
+
+    @api changeBarColor3() {
+
+        this.className3 = "redBar";
+    }
+
+    @api changeBarColor4() {
+
+        this.className4= "redBar";
+    }
+
+    
+
+    handleButtonClick1(){
+
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    handleButtonClick2(){
+
+        this.dispatchEvent(new CustomEvent('close2'));
+    }
+    handleButtonClick3(){
+
+        this.dispatchEvent(new CustomEvent('close3'));
+    }
+    handleButtonClick4(){
+
+        this.dispatchEvent(new CustomEvent('close4'));
+    }
+}
